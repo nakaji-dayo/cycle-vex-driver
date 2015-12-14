7deb435c05216da8185bcf6950cc94b9ba8e8e00
nakaji-dayo <nakaji.dayo@gmail.com>
2015-12-15 02:29:26 +0900

    add driver

---
+++ index.js
@@ -0,0 +1,13 @@
+import Rx from 'rx';
+import vex from 'vex-js/js/vex.dialog.js';
+
+export default function makeVexDriver(className) {
+    vex.defaultOptions.className = className;
+    return function vexDriver(open$) {	
+	let _vex = Rx.Observable.fromCallback((id, method, options, cb) => {
+	    let _cb = (v) => cb({value: v, id: id});
+	    return vex[method](_.assign(options, {callback: _cb}));
+	});
+	return open$.flatMap(open => _vex(open.id, open.method || 'open', open.options));
+    };
+};
